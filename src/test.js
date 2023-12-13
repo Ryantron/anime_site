07a6ab8cdccc3fb995eb9fd4e3da2a0f77f9b7b4
mkowalczyk570 <blockman570@gmail.com>
2023-12-12 19:07:01 -0500

    Added logic for making, accepting, rejecting friend requests. Also added functionality to remove friends. Code is a little messy and can be cleaned up

--- src/test.js
+++ src/test.js
@@ -1,8 +1,6 @@
 import { getManualListRecs, getUserRecs, rateRecommendations } from "./data/recommendations.js";
 import { unlinkMalAccount, linkMalAccount } from "./data/users.js";
 import { registerUser } from "./data/users.js";
+import { sendFriendRequest, acceptFriendRequest, rejectFriendRequest, removeFriend } from "./data/friends.js";
 
-console.log(await rateRecommendations("blockman57@gmail.com", "657659250d02c7af77a8322a", 5))
-
-console.log(await rateRecommendations("blockman57@gmail.com", "657659250d02c7af77a8322a", 2))
-
+console.log(await removeFriend("test", "aero"))
